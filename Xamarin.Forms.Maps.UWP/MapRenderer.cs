ebf6eee6e32ff914c49afbb3a3c83f4686d61d23
Christophe <Christophe@DESKTOP-DDUR6NL>
2017-01-26 20:22:17 -0800

    Issues: 1. The MapRenderer calls TrySetViewBoundsAsync before the map is ready for it. This causes the map to hardly ever initialize to the correct position. 2. The visible region calculation gets the zoom wrong. 3. Map can cause a crash if you rotate/zoom a lot when setting the visible region.
    Fixes:
    1. Call MoveToRegion using MapControl's RunIdleAsync to have it move after the map starts rendering
    2. lat/long delta should be:
    var latitudeDelta = Math.Abs(nw.Position.Latitude - se.Position.Latitude);
    var longitudeDelta = Math.Abs(nw.Position.Longitude - se.Position.Longitude);
    rather than calculated from the center
    3. Move the visible region set into the try/catch block.

--- Xamarin.Forms.Maps.UWP/MapRenderer.cs
+++ Xamarin.Forms.Maps.UWP/MapRenderer.cs
@@ -26,285 +26,280 @@ namespace Xamarin.Forms.Maps.UWP
 namespace Xamarin.Forms.Maps.WinRT
 #endif
 {
-	public class MapRenderer : ViewRenderer<Map, MapControl>
-	{
-		protected override async void OnElementChanged(ElementChangedEventArgs<Map> e)
-		{
-			base.OnElementChanged(e);
+    public class MapRenderer : ViewRenderer<Map, MapControl>
+    {
+        protected override async void OnElementChanged(ElementChangedEventArgs<Map> e)
+        {
+            base.OnElementChanged(e);
 
-			if (e.OldElement != null)
-			{
-				var mapModel = e.OldElement;
-				MessagingCenter.Unsubscribe<Map, MapSpan>(this, "MapMoveToRegion");
-				((ObservableCollection<Pin>)mapModel.Pins).CollectionChanged -= OnCollectionChanged;
-			}
+            if (e.OldElement != null)
+            {
+                var mapModel = e.OldElement;
+                MessagingCenter.Unsubscribe<Map, MapSpan>(this, "MapMoveToRegion");
+                ((ObservableCollection<Pin>)mapModel.Pins).CollectionChanged -= OnCollectionChanged;
+            }
 
-			if (e.NewElement != null)
-			{
-				var mapModel = e.NewElement;
+            if (e.NewElement != null)
+            {
+                var mapModel = e.NewElement;
 
-				if (Control == null)
-				{
-					SetNativeControl(new MapControl());
-					Control.MapServiceToken = FormsMaps.AuthenticationToken;
-					Control.ZoomLevelChanged += async (s, a) => await UpdateVisibleRegion();
-					Control.CenterChanged += async (s, a) => await UpdateVisibleRegion();
-				}
+                if (Control == null)
+                {
+                    SetNativeControl(new MapControl());
+                    Control.MapServiceToken = FormsMaps.AuthenticationToken;
+                    Control.ZoomLevelChanged += UpdateVisibleRegion;
+                    Control.CenterChanged += UpdateVisibleRegion;
+                }
 
-				MessagingCenter.Subscribe<Map, MapSpan>(this, "MapMoveToRegion", async (s, a) => await MoveToRegion(a), mapModel);
+                MessagingCenter.Subscribe<Map, MapSpan>(this, "MapMoveToRegion", async (s, a) => await MoveToRegion(a), mapModel);
 
-				UpdateMapType();
-				UpdateHasScrollEnabled();
-				UpdateHasZoomEnabled();
+                UpdateMapType();
+                UpdateHasScrollEnabled();
+                UpdateHasZoomEnabled();
 
-				((ObservableCollection<Pin>)mapModel.Pins).CollectionChanged += OnCollectionChanged;
+                ((ObservableCollection<Pin>)mapModel.Pins).CollectionChanged += OnCollectionChanged;
 
-				if (mapModel.Pins.Any())
-					LoadPins();
+                if (mapModel.Pins.Any())
+                    LoadPins();
 
-				await UpdateIsShowingUser();
-			}
-		}
+                await UpdateIsShowingUser();
+                await Control.Dispatcher.RunIdleAsync(async (i) => await MoveToRegion(mapModel.LastMoveToRegion, MapAnimationKind.None));
+            }
+        }
 
-		protected override async void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
-		{
-			base.OnElementPropertyChanged(sender, e);
+        protected override async void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
 
-			if (e.PropertyName == Map.MapTypeProperty.PropertyName)
-				UpdateMapType();
-			else if (e.PropertyName == Map.IsShowingUserProperty.PropertyName)
-				await UpdateIsShowingUser();
-			else if (e.PropertyName == Map.HasScrollEnabledProperty.PropertyName)
-				UpdateHasScrollEnabled();
-			else if (e.PropertyName == Map.HasZoomEnabledProperty.PropertyName)
-				UpdateHasZoomEnabled();
-		}
+            if (e.PropertyName == Map.MapTypeProperty.PropertyName)
+                UpdateMapType();
+            else if (e.PropertyName == Map.IsShowingUserProperty.PropertyName)
+                await UpdateIsShowingUser();
+            else if (e.PropertyName == Map.HasScrollEnabledProperty.PropertyName)
+                UpdateHasScrollEnabled();
+            else if (e.PropertyName == Map.HasZoomEnabledProperty.PropertyName)
+                UpdateHasZoomEnabled();
+        }
 
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && !_disposed)
-			{
-				_disposed = true;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && !_disposed)
+            {
+                _disposed = true;
 
-				_timer?.Stop();
-				_timer = null;
+                _timer?.Stop();
+                _timer = null;
 
-				MessagingCenter.Unsubscribe<Map, MapSpan>(this, "MapMoveToRegion");
+                MessagingCenter.Unsubscribe<Map, MapSpan>(this, "MapMoveToRegion");
 
-				if (Element != null)
-					((ObservableCollection<Pin>)Element.Pins).CollectionChanged -= OnCollectionChanged;
-			}
-			base.Dispose(disposing);
-		}
+                if (Element != null)
+                    ((ObservableCollection<Pin>)Element.Pins).CollectionChanged -= OnCollectionChanged;
+            }
+            base.Dispose(disposing);
+        }
 
-		bool _disposed;
-		bool _firstZoomLevelChangeFired;
-		Ellipse _userPositionCircle;
-		DispatcherTimer _timer;
+        bool _disposed;
+        Ellipse _userPositionCircle;
+        DispatcherTimer _timer;
 
-		void OnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-		{
-			switch (e.Action)
-			{
-				case NotifyCollectionChangedAction.Add:
-					foreach (Pin pin in e.NewItems)
-						LoadPin(pin);
-					break;
-				case NotifyCollectionChangedAction.Move:
-					// no matter
-					break;
-				case NotifyCollectionChangedAction.Remove:
-					foreach (Pin pin in e.OldItems)
-						RemovePin(pin);
-					break;
-				case NotifyCollectionChangedAction.Replace:
-					foreach (Pin pin in e.OldItems)
-						RemovePin(pin);
-					foreach (Pin pin in e.NewItems)
-						LoadPin(pin);
-					break;
-				case NotifyCollectionChangedAction.Reset:
-					ClearPins();
-					break;
-			}
-		}
+        void OnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    foreach (Pin pin in e.NewItems)
+                        LoadPin(pin);
+                    break;
+                case NotifyCollectionChangedAction.Move:
+                    // no matter
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    foreach (Pin pin in e.OldItems)
+                        RemovePin(pin);
+                    break;
+                case NotifyCollectionChangedAction.Replace:
+                    foreach (Pin pin in e.OldItems)
+                        RemovePin(pin);
+                    foreach (Pin pin in e.NewItems)
+                        LoadPin(pin);
+                    break;
+                case NotifyCollectionChangedAction.Reset:
+                    ClearPins();
+                    break;
+            }
+        }
 
-		void LoadPins()
-		{
-			foreach (var pin in Element.Pins)
-				LoadPin(pin);
-		}
+        void LoadPins()
+        {
+            foreach (var pin in Element.Pins)
+                LoadPin(pin);
+        }
 
-		void ClearPins()
-		{
-			Control.Children.Clear();
+        void ClearPins()
+        {
+            Control.Children.Clear();
 #pragma warning disable 4014 // don't wanna block UI thread
-			UpdateIsShowingUser();
+            UpdateIsShowingUser();
 #pragma warning restore
-		}
+        }
 
-		void RemovePin(Pin pinToRemove)
-		{
-			var pushPin = Control.Children.FirstOrDefault(c =>
-			{
-				var pin = (c as PushPin);
-				return (pin != null && pin.DataContext.Equals(pinToRemove));
-			});
+        void RemovePin(Pin pinToRemove)
+        {
+            var pushPin = Control.Children.FirstOrDefault(c =>
+            {
+                var pin = (c as PushPin);
+                return (pin != null && pin.DataContext.Equals(pinToRemove));
+            });
 
-			if (pushPin != null)
-				Control.Children.Remove(pushPin);
-		}
+            if (pushPin != null)
+                Control.Children.Remove(pushPin);
+        }
 
-		void LoadPin(Pin pin)
-		{
-			Control.Children.Add(new PushPin(pin));
-		}
+        void LoadPin(Pin pin)
+        {
+            Control.Children.Add(new PushPin(pin));
+        }
 
-		async Task UpdateIsShowingUser(bool moveToLocation = true)
-		{
-			
-			if (Element.IsShowingUser)
-			{
-				var myGeolocator = new Geolocator();
-				if (myGeolocator.LocationStatus != PositionStatus.NotAvailable &&
-				    myGeolocator.LocationStatus != PositionStatus.Disabled)
-				{
-					var userPosition = await myGeolocator.GetGeopositionAsync();
-					if (userPosition?.Coordinate != null)
-						LoadUserPosition(userPosition.Coordinate, moveToLocation);
-				}
+        async Task UpdateIsShowingUser(bool moveToLocation = true)
+        {
 
-				if (_timer == null)
-				{
-					_timer = new DispatcherTimer();
-					_timer.Tick += async (s, o) => await UpdateIsShowingUser(moveToLocation: false);
-					_timer.Interval = TimeSpan.FromSeconds(15);
-				}
-				
-				if (!_timer.IsEnabled)
-					_timer.Start();
-			}
-			else if (_userPositionCircle != null && Control.Children.Contains(_userPositionCircle))
-			{
-				_timer?.Stop();
-				Control.Children.Remove(_userPositionCircle);
-			}
-		}
+            if (Element.IsShowingUser)
+            {
+                var myGeolocator = new Geolocator();
+                if (myGeolocator.LocationStatus != PositionStatus.NotAvailable &&
+                    myGeolocator.LocationStatus != PositionStatus.Disabled)
+                {
+                    var userPosition = await myGeolocator.GetGeopositionAsync();
+                    if (userPosition?.Coordinate != null)
+                        LoadUserPosition(userPosition.Coordinate, moveToLocation);
+                }
 
-		async Task MoveToRegion(MapSpan span, MapAnimationKind animation = MapAnimationKind.Bow)
-		{
-			var nw = new BasicGeoposition
-			{
-				Latitude = span.Center.Latitude + span.LatitudeDegrees / 2,
-				Longitude = span.Center.Longitude - span.LongitudeDegrees / 2
-			};
-			var se = new BasicGeoposition
-			{
-				Latitude = span.Center.Latitude - span.LatitudeDegrees / 2,
-				Longitude = span.Center.Longitude + span.LongitudeDegrees / 2
-			};
-			var boundingBox = new GeoboundingBox(nw, se);
-			await Control.TrySetViewBoundsAsync(boundingBox, null, animation);
-		}
+                if (_timer == null)
+                {
+                    _timer = new DispatcherTimer();
+                    _timer.Tick += async (s, o) => await UpdateIsShowingUser(moveToLocation: false);
+                    _timer.Interval = TimeSpan.FromSeconds(15);
+                }
 
-		async Task UpdateVisibleRegion()
-		{
-			if (Control == null || Element == null)
-				return;
+                if (!_timer.IsEnabled)
+                    _timer.Start();
+            }
+            else if (_userPositionCircle != null && Control.Children.Contains(_userPositionCircle))
+            {
+                _timer?.Stop();
+                Control.Children.Remove(_userPositionCircle);
+            }
+        }
 
-			if (!_firstZoomLevelChangeFired)
-			{
-				await MoveToRegion(Element.LastMoveToRegion, MapAnimationKind.None);
-				_firstZoomLevelChangeFired = true;
-				return;
-			}
-			Geopoint nw, se = null;
-			try
-			{
-				Control.GetLocationFromOffset(new Windows.Foundation.Point(0, 0), out nw);
-				Control.GetLocationFromOffset(new Windows.Foundation.Point(Control.ActualWidth, Control.ActualHeight), out se);
-			}
-			catch (Exception)
-			{
-				return;
-			}
+        async Task MoveToRegion(MapSpan span, MapAnimationKind animation = MapAnimationKind.Bow)
+        {
+            var nw = new BasicGeoposition
+            {
+                Latitude = span.Center.Latitude + span.LatitudeDegrees / 2,
+                Longitude = span.Center.Longitude - span.LongitudeDegrees / 2
+            };
+            var se = new BasicGeoposition
+            {
+                Latitude = span.Center.Latitude - span.LatitudeDegrees / 2,
+                Longitude = span.Center.Longitude + span.LongitudeDegrees / 2
+            };
+            var boundingBox = new GeoboundingBox(nw, se);
+            await Control.TrySetViewBoundsAsync(boundingBox, null, animation);
+        }
 
-			if (nw != null && se != null)
-			{
-				var boundingBox = new GeoboundingBox(nw.Position, se.Position);
-				var center = new Position(boundingBox.Center.Latitude, boundingBox.Center.Longitude);
-				var latitudeDelta = Math.Abs(center.Latitude - boundingBox.NorthwestCorner.Latitude);
-				var longitudeDelta = Math.Abs(center.Longitude - boundingBox.NorthwestCorner.Longitude);
-				Element.VisibleRegion = new MapSpan(center, latitudeDelta, longitudeDelta);
-			}
-		}
+        private void UpdateVisibleRegion(MapControl sender, object args)
+        {
+            if (Control == null || Element == null)
+                return;
 
-		void LoadUserPosition(Geocoordinate userCoordinate, bool center)
-		{
-			var userPosition = new BasicGeoposition
-			{
-				Latitude = userCoordinate.Point.Position.Latitude,
-				Longitude = userCoordinate.Point.Position.Longitude
-			};
 
-			var point = new Geopoint(userPosition);
+            try
+            {
+                Geopoint nw, se = null;
+                Control.GetLocationFromOffset(new Windows.Foundation.Point(0, 0), out nw);
+                Control.GetLocationFromOffset(new Windows.Foundation.Point(Control.ActualWidth, Control.ActualHeight), out se);
+                if (nw != null && se != null)
+                {
+                    var boundingBox = new GeoboundingBox(nw.Position, se.Position);
+                    var center = new Position(boundingBox.Center.Latitude, boundingBox.Center.Longitude);
+                    var latitudeDelta = Math.Abs(nw.Position.Latitude - se.Position.Latitude);
+                    var longitudeDelta = Math.Abs(nw.Position.Longitude - se.Position.Longitude);
+                    Element.VisibleRegion = new MapSpan(center, latitudeDelta, longitudeDelta);
+                }
+            }
+            catch (Exception)
+            {
+                return;
+            }
 
-			if (_userPositionCircle == null)
-			{
-				_userPositionCircle = new Ellipse
-				{
-					Stroke = new SolidColorBrush(Colors.White),
-					Fill = new SolidColorBrush(Colors.Blue),
-					StrokeThickness = 2,
-					Height = 20,
-					Width = 20,
-					Opacity = 50
-				};
-			}
+        }
 
-			if (Control.Children.Contains(_userPositionCircle))
-				Control.Children.Remove(_userPositionCircle);
+        void LoadUserPosition(Geocoordinate userCoordinate, bool center)
+        {
+            var userPosition = new BasicGeoposition
+            {
+                Latitude = userCoordinate.Point.Position.Latitude,
+                Longitude = userCoordinate.Point.Position.Longitude
+            };
 
-			MapControl.SetLocation(_userPositionCircle, point);
-			MapControl.SetNormalizedAnchorPoint(_userPositionCircle, new Windows.Foundation.Point(0.5, 0.5));
+            var point = new Geopoint(userPosition);
 
-			Control.Children.Add(_userPositionCircle);
+            if (_userPositionCircle == null)
+            {
+                _userPositionCircle = new Ellipse
+                {
+                    Stroke = new SolidColorBrush(Colors.White),
+                    Fill = new SolidColorBrush(Colors.Blue),
+                    StrokeThickness = 2,
+                    Height = 20,
+                    Width = 20,
+                    Opacity = 50
+                };
+            }
 
-			if (center)
-			{
-				Control.Center = point;
-				Control.ZoomLevel = 13;
-			}
-		}
+            if (Control.Children.Contains(_userPositionCircle))
+                Control.Children.Remove(_userPositionCircle);
 
-		void UpdateMapType()
-		{
-			switch (Element.MapType)
-			{
-				case MapType.Street:
-					Control.Style = MapStyle.Road;
-					break;
-				case MapType.Satellite:
-					Control.Style = MapStyle.Aerial;
-					break;
-				case MapType.Hybrid:
-					Control.Style = MapStyle.AerialWithRoads;
-					break;
-			}
-		}
+            MapControl.SetLocation(_userPositionCircle, point);
+            MapControl.SetNormalizedAnchorPoint(_userPositionCircle, new Windows.Foundation.Point(0.5, 0.5));
+
+            Control.Children.Add(_userPositionCircle);
+
+            if (center)
+            {
+                Control.Center = point;
+                Control.ZoomLevel = 13;
+            }
+        }
+
+        void UpdateMapType()
+        {
+            switch (Element.MapType)
+            {
+                case MapType.Street:
+                    Control.Style = MapStyle.Road;
+                    break;
+                case MapType.Satellite:
+                    Control.Style = MapStyle.Aerial;
+                    break;
+                case MapType.Hybrid:
+                    Control.Style = MapStyle.AerialWithRoads;
+                    break;
+            }
+        }
 
 #if WINDOWS_UWP
-		void UpdateHasZoomEnabled()
-		{
-			Control.ZoomInteractionMode = Element.HasZoomEnabled
-				? MapInteractionMode.GestureAndControl
-				: MapInteractionMode.ControlOnly;
-		}
+        void UpdateHasZoomEnabled()
+        {
+            Control.ZoomInteractionMode = Element.HasZoomEnabled
+                ? MapInteractionMode.GestureAndControl
+                : MapInteractionMode.ControlOnly;
+        }
 
-		void UpdateHasScrollEnabled()
-		{
-			Control.PanInteractionMode = Element.HasScrollEnabled ? MapPanInteractionMode.Auto : MapPanInteractionMode.Disabled;
-		}
+        void UpdateHasScrollEnabled()
+        {
+            Control.PanInteractionMode = Element.HasScrollEnabled ? MapPanInteractionMode.Auto : MapPanInteractionMode.Disabled;
+        }
 #else
 		void UpdateHasZoomEnabled()
 		{
@@ -314,5 +309,5 @@ namespace Xamarin.Forms.Maps.WinRT
 		{
 		}
 #endif
-	}
+    }
 }
